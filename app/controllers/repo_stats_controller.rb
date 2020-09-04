1d5b8477204c941dcabfbcab068c0eb499f755db
Brandon Sotiroff <bsotiroff1@gmail.com>
2020-09-03 19:40:01 -0500

    add controller skeleton for post actions

--- app/controllers/repo_stats_controller.rb
+++ app/controllers/repo_stats_controller.rb
@@ -2,6 +2,7 @@
 
 class RepoStatsController < ApplicationController
   def index
+
     vote_counts = {}
     Framework.all.each do |f|
       vote_counts[f.name] = f.vote_count
@@ -9,5 +10,17 @@ class RepoStatsController < ApplicationController
     configs = { voteCounts: vote_counts }
     @configs = configs.to_json
     pp @configs
+  end
+
+  def vote(repo_name)
+    framework = Framework.find_by(name: repo_name)
+
+    framework.vote_count.increment!
+  end
+
+  def fetch_api_data
+
+    RestClient.get('https://api.github.com/repos/facebook/react', headers={Authorization: "token #{Application.secrets.github_api_token}"})
+
   end
 end
